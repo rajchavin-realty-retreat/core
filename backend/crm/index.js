827b46995d0185d13f4026eb9c10ec932d2f365d
surya3444 <isuryakarthikvarma@gmail.com>
2026-04-27 22:30:16 +0530

    changed email domain

--- backend/crm/index.js
+++ backend/crm/index.js
@@ -2,10 +2,17 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const authRoutes = require('./routes/authRoutes');
 
 
 const app = express();
+
+app.use((req, res, next) => {
+  console.log(`[INCOMING REQUEST] ${req.method} ${req.url}`);
+  next();
+});
+
+const authRoutes = require('./routes/authRoutes');
+
 
 // --- THE SECURITY BOUNCER (CORS WHITELIST) ---
 const whitelist = [
